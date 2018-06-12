cdda91c24b2c23447e38f20ddf55e9030ce02cd3
Gabriel Rufino <email@gabrielrufino.com>
2018-06-12 11:04:43 -0300

    Initial log system

--- GeneseBot.js
+++ GeneseBot.js
@@ -1,12 +1,16 @@
-module.exports = () => {  
+module.exports = () => {
   const bot = require('./src/bot')
-  const response = require('./src/response')
-  
+  const log = require('./src/log')
+  const response = require('./src/response')  
+
   bot.onText(/\/bitcoin/, async (msg) => {
     const chatId = msg.chat.id
     const resp = await response.bitcoin()
   
     bot.sendMessage(chatId, resp)
+      .then(() => {
+        log.request('bitcoin')
+      })
   })
   
   bot.onText(/\/cep (.+)/, async (msg, match) => {
@@ -14,6 +18,9 @@ module.exports = () => {
     const resp = await response.cep(match[1])
   
     bot.sendMessage(chatId, resp)
+      .then(() => {
+        log.request('cep')
+      })
   })
   
   bot.onText(/\/date/, (msg) => {
@@ -21,6 +28,9 @@ module.exports = () => {
     const resp = response.date()
   
     bot.sendMessage(chatId, resp)
+      .then(() => {
+        log.request('date')
+      })
   })
   
   bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -28,6 +38,9 @@ module.exports = () => {
     const resp = response.echo(match)
   
     bot.sendMessage(chatId, resp)
+      .then(() => {
+        log.request('echo')
+      })
   })
   
   bot.onText(/\/help/, (msg) => {
@@ -35,14 +48,18 @@ module.exports = () => {
     const resp = response.help
   
     bot.sendMessage(chatId, resp)
+      .then(() => {
+        log.request('help')
+      })
   })
   
   bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id
     const resp = response.start(msg)
 
-    bot.sendPhoto(chatId, resp.profile, {
-      caption: resp.message
-    })
+    bot.sendPhoto(chatId, resp.profile, {caption: resp.message})
+      .then(() => {
+        log.request('start')
+      })
   })
 }
