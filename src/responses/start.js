c5af20f54ddb52913aedc306e4092ce13b8092d4
Gabriel Rufino <contato@gabrielrufino.com>
2019-02-16 22:50:11 -0300

    public -> assets; assets/imgs -> assets/images

--- src/responses/start.js
+++ src/responses/start.js
@@ -1,6 +1,6 @@
 const start = (msg) => {
   return {
-    profile: './public/imgs/profile.jpg',
+    profile: './assets/images/profile.jpg',
     message: `Hello, ${msg.from.first_name}! My name is GeneseBot and i'm a chatbot ready to the hard work... any work :D Nice to meet you!`
   }
 }
